aab8a597dfae952917837cb6fc9e21ffb6bb5376
IvanCodeGit <email@email.com>
2024-04-08 12:16:12 +0300

    First Commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from "expo-status-bar";
+import { FC } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const App: FC = () => {
+  return (
+    <View>
+      <StatusBar style="auto" />
+    </View>
+  );
+};

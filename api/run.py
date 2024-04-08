83f8de413d54e021946a6a288303e3ac8406b758
Zaskilovan <diulgervanya@gmail.com>
2024-04-08 23:45:35 +0200

    updates

--- api/run.py
+++ api/run.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile , Query
+from typing import Optional
+from src import Scaner
 
 app = FastAPI()
 
@@ -6,9 +8,16 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/scan_photo", status_code=200)
+async def scan_photo(
+    photo: UploadFile,
+    longitude: Optional[float] = Query(None, title="Longitude", description="Longitude of the photo", ge=-180, le=180), 
+    latitude: Optional[float] = Query(None, title="Latitude", description="Latitude of the photo", ge=-90, le=90),
+    ):
+
+    print(1)                 
+    return await Scaner.scan_photo(photo, longitude, latitude)
+
 
 
 
---
+++ api/src/photo_scaning.py
@@ -0,0 +1,73 @@
+from fastapi import UploadFile
+from typing import Optional
+from base64 import b64encode
+
+from fastapi.responses import FileResponse
+
+import cv2
+from ultralytics import YOLO
+import supervision as sv
+import numpy as np
+import pprint
+
+from uuid import uuid4
+
+objects = {
+    "Opera theatre" : { 
+        "latitude" : 46.485058,
+        "longitude" : 30.741136,
+    },
+    "The Potemkin Stairs":{
+        "latitude" : 46.488868,
+        "longitude" : 30.742358,
+    }
+}, 
+
+
+class ScanPhoto:
+
+    def __init__(self):
+        self.model = YOLO("weights/yolov8n.pt")
+        self.box_annotator = sv.BoxAnnotator(
+            thickness=2,
+            text_thickness=2,
+            text_scale=1 )
+    
+    async def scan_photo(self, photo: UploadFile, longitude: Optional[float], latitude: Optional[float]):
+
+        # Read the photo
+        contents = await photo.read()
+
+        # Преобразуем массив байтов в массив numpy
+        nparr = np.frombuffer(contents, np.uint8)
+
+        # Декодируем массив numpy в кадр OpenCV
+        frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+        result = self.model(frame, agnostic_nms=True)[0]
+
+
+        detections = sv.Detections.from_ultralytics(result)
+        labels = [
+            f"{self.model.model.names[class_id]} {confidence:0.2f}"
+            for _, _, confidence, class_id, *_
+            in detections
+        ]
+
+        frame = self.box_annotator.annotate(
+                        scene=frame, 
+                        detections=detections, 
+                        labels=labels
+                    )
+        
+        path = f"media/{uuid4()}.jpg"
+        cv2.imwrite(path, frame)
+
+        return FileResponse(path, media_type="image/jpeg")
+    
+
+Scaner = ScanPhoto()
+
+
+
+
